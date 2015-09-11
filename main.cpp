11b0c2d890976e76a9215673d5decf89a759e028
Pitt05 <pierre.bouffier05@gmail.com>
2015-09-11 14:03:48 +0200

    Première edition main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
+/**
+ *@author Pierre Bouffier
+ *@version 1.0.0
+ *@date 11/09/2015
+ *@file main.cpp
+ */
+
+
 #include <QCoreApplication>
+
+/**
+ * @brief main Fonction principale d'où le programme découlera
+ * @param argc Nombre d'arguments
+ * @param argv Valeur des arguments
+ * @return return l'execution du QCoreApplication
+ */
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
+
+
     return a.exec();
 }
